32d9adc33af75c0a18357379ad0c98f383558129
Phinatech <ugbojudithchinemerem@gmil.com>
2023-03-15 13:03:14 +0100

    backend

--- src/Middlewares/validator/userValidator/userValidator.ts
+++ src/Middlewares/validator/userValidator/userValidator.ts
@@ -1,1 +1,10 @@
 import { RequestHandler } from "express";
+import { validator } from "../validator";
+import { userSchema } from "./userSchema";
+
+//validato is expecting three parameter
+export const registerValidator: RequestHandler = (req, res, next) =>
+  validator(userSchema.register, req.body, next);
+
+export const loginValidator: RequestHandler = (req, res, next) =>
+  validator(userSchema.login, req.body, next);
